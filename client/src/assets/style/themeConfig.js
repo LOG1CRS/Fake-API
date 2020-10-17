c91b786c6334f10ada2fcde1002ca6dd80617280
André Marco Ruiz Sandoval <log1c.rs.psc@gmail.com>
2020-10-16 21:01:28 -0500

    theme config finished, new routes addedm and tab icon added

--- client/src/assets/style/themeConfig.js
+++ client/src/assets/style/themeConfig.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#f4cd04',
+      main: '#fbc02d',
     },
     secondary: {
       main: '#f7f7f7',
